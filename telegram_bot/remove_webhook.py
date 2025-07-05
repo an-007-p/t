d3bf264d5185e76ba7a0ff9afd3435fb1624669e
an-007-p <an.novoselova007@gmail.com>
2025-07-05 13:12:04 +0300

    Delete telegram_bot directory

--- telegram_bot/remove_webhook.py
+++
@@ -1,9 +0,0 @@
-from telegram import Bot
-import asyncio
-
-async def main():
-    bot = Bot(token="2007814209:AAEfgXKP3vRb3czgExrYUik0G0iIZxsX40w")
-    await bot.delete_webhook()
-    print("Webhook снят!")
-
-asyncio.run(main())
